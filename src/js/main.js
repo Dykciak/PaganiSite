2bc7c08864866d9d371e07e6b07c4ef4a6e8c8b3
dykciak <dawid.dykto@gmail.com>
2025-01-16 21:28:53 +0000

    before changes

--- src/js/main.js
+++ src/js/main.js
@@ -7,7 +7,6 @@ let buttons; // przyciski karuzeli
 const main = () => {
 	prepareDOMElements();
 	prepareDOMEvents();
-	startCarouselAutoPlay();
 };
 
 // pobieranie wszystkich elementów
@@ -48,31 +47,19 @@ const handleNavItemsAnimation = () => {
 	});
 };
 
-// obsługa karuzeli
-const handleCarousel = (e) => {
-	const offset = e.target.dataset.carouselButton === "right" ? 1 : -1; // określa kierunek przesunięcia
-	moveCarousel(offset);
-};
 
-// automatyczne przewijanie karuzeli
-const startCarouselAutoPlay = () => {
-	carouselInterval = setInterval(() => {
-		moveCarousel(1); // przesuwa karuzelę o jeden slajd w prawo co 10 sekund
-	}, 10000);
-};
-
-// przesuwanie karuzeli
-const moveCarousel = (offset) => {
-	const slides = document.querySelector("[data-slides]"); // znajduje listę slajdów
-	const activeSlide = slides.querySelector("[data-active]"); // znajduje aktywny slajd
-	let newIndex = [...slides.children].indexOf(activeSlide) + offset;
-
-	// ustawia nowy indeks, obsługując początek i koniec listy slajdów
-	if (newIndex < 0) newIndex = slides.children.length - 1;
-	if (newIndex >= slides.children.length) newIndex = 0;
-
-	slides.children[newIndex].dataset.active = true; // ustawia nowy aktywny slajd
-	delete activeSlide.dataset.active; // usuwa status aktywności z poprzedniego slajdu
-};
+var swiper = new Swiper(".mySwiper", {
+	slidesPerView: 1,
+	spaceBetween: 30,
+	loop: true,
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+});
 
 document.addEventListener("DOMContentLoaded", main);
